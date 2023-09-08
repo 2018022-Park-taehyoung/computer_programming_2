2302bf91740a233e1be506e9c63bee81c482ed64
2018022_Park_taehyoung <144421032+2018022-Park-taehyoung@users.noreply.github.com>
2023-09-08 16:36:54 +0900

    2018022_Quiz
    "안녕하세요 XX님. 입력하신 두 숫자는 각각 A, B입니다." 출력 코드

---
+++ 2018022_Quiz.py
@@ -0,0 +1,4 @@
+name = input("이름을 입력하세요: ")
+number_one = input("첫 번째 숫자를 입력하세요: ")
+number_two = input("두 번째 숫자를 입력하세요: ")
+print("안녕하세요. " + name + "님. 입력하신 두 숫자는 각각 " + number_one + "," + number_two + "입니다.")
